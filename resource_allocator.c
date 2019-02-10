25745410fd90bf39600a25863fb6e08de711e7cb
Shaleen Arora <shaleenarora4@gmail.com>
2019-02-10 23:47:42 +0530

    Update resource_allocator.c

--- resource_allocator.c
+++ resource_allocator.c
@@ -1,5 +1,3 @@
-//There are two primary participants; One Teacher(T) and three students(S1,S2 and S3). A student needs four resources to complete a drawing: pencil,paper,sharpener and eraser.Initially, S1 is issued a paper,sharpener and eraser. S2 is issued paper,pencil and eraser. S3 is issued paper,eraser and pencil.Teacher puts any one of the three resources(pencil,eraser and sharpener) on a table and all three students try to take that resource. Only one student will be able to complete the drawing. Teacher keeps repeating the process until all of the students completed their drawing operation.
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
